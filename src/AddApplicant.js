c157c4f4ec1ec7a91d8a6b171778d64f498b9e86
shinyuy <shinyuy9@gmail.com>
2019-03-18 18:26:07 +0100

    First commit with Loan Application from set up

---
+++ src/AddApplicant.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react'
+
+export default class AddApplicant extends Component {
+   
+    state = {
+        name: null,
+        age: null,
+        location: null,
+        number: null,
+        amount: null,
+        colateral: null,
+        message: null,
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(this.state);
+    }
+
+    render() {
+        return (
+                <div className='row appform'>
+                 <h1>Apply For Loan</h1>
+                    <form onSubmit={this.handleSubmit} className='col s12 center'>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <label htmlFor='name'>Full Names : </label>
+                                <input type='text' placeholder='Enter full names as on ID card here' id='name' onChange={this.handleChange} />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <label htmlFor='age'>Age : </label>
+                                <input type='number' placeholder='Enter your age here' id='age' onChange={this.handleChange} />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <label htmlFor='location'>Location : </label>
+                                <input type='text' placeholder='Enter your resident location here' id='location' onChange={this.handleChange} />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <label htmlFor='number'>Phone Number : </label>
+                                <input type='number' placeholder='Enter you main phone number here' id='number' onChange={this.handleChange} />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <label htmlFor='amount'>Amount : </label>
+                                <input type='number' placeholder='Enter the loan amount in XAF you are applying for here' id='amount' onChange={this.handleChange} />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <label htmlFor='colateral'>Colateral Property : </label>
+                                <input type='text' placeholder='Enter any property you have as colateral here' id='colateral' onChange={this.handleChange} />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <label htmlFor='message'>Tell us why you need this loan : </label>
+                                <textarea type='text' placeholder='Enter reason for loan here' id='message' cols='10' rows='20' onChange={this.handleChange} ></textarea>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <button type='submit'>Submit Loan Application</button>
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
+
+        )
+    }
+}
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import AddApplicant from './AddApplicant';
+
+class App extends Component {
+
+  state = {
+    applicants : [
+      { id: 1 , name: 'John', age: 30, location : 'nkwen', phoneNumber : 72727272, amount : 100000, colateral: '20km land at nkwen', message : 'Hello I would like to apply for a loan to carry out my project'},
+      { id: 2 , name: 'George', age: 25, location : 'bambui', phoneNumber : 62007272, amount : 150000,  colateral: '5 bedroom house', message : 'Hello I would like to apply for a loan to pay my school fees'},
+      { id: 3 , name: 'Jude', age: 33, location : 'sonacstreet', phoneNumber : 99577272, amount : 1000000,  colateral: 'a toyota corolla', message : 'Hello I would like tto apply for a loan start up a small business'},
+      { id: 4 , name: 'Mary', age: 36, location : 'aziri', phoneNumber : 727290095, amount : 250000,  colateral: '10km land at aziri ', message : 'Hello I would like tto apply for a loan to carry out my project'}
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+       <AddApplicant />
+      </div>
+    );
+  }
+}
+
+export default App;
