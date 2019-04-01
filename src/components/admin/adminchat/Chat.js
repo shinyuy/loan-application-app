6d51e993359c659d182d336a7ca1c86d5bda9239
shinyuy <shinyuy9@gmail.com>
2019-04-01 02:06:45 +0100

    Added chat functionality, for credit union workers to collaborate through messages when on the app

--- src/components/admin/adminchat/Chat.js
+++ src/components/admin/adminchat/Chat.js
@@ -1,28 +1,73 @@
-import React, { Component } from 'react';
-import './chat.css';
-import io from 'socket.io-client';
- 
-
-
+import React, { Component } from "react";
+import io from "socket.io-client";
 
 export default class Chat extends Component {
+    constructor(props) {
+        super(props);
 
-    componentDidMount(){
-        const socket = io('http://localhost:4000');
+        this.state = {
+            username: '',
+            message: '',
+            messages: []
+        };
+
+        this.socket = io('localhost:4000');
+
+        this.socket.on('RECEIVE_MESSAGE', function (data) {
+            addMessage(data);
+        });
+
+        const addMessage = data => {
+            console.log(data);
+            this.setState({ messages: [...this.state.messages, data] });
+            console.log(this.state.messages);
+        };
+
+        this.sendMessage = e => {
+            e.preventDefault();
+            this.socket.emit('SEND_MESSAGE', {
+                author: this.state.username,
+                message: this.state.message
+            })
+            this.setState({ message: '' });
+
+        }
     }
-
     render() {
         return (
-            <div id='chat'>
-            <div id='chat-window'>
-               <div id='output'></div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-4">
+                        <div className="card">
+                            <div className="card-body">
+                                <div className="card-title">Chat</div>
+                                <hr />
+                                <div className="messages" 
+                                style={{
+                                    overflowY : 'scroll',
+                                    height :'200px'
+                                    }} >
+                                    {this.state.messages.map(message => {
+                                        return (
+                                            <div>{message.author}: {message.message}</div>
+                                        )
+                                    })}
+                                </div>
+
+                            </div>
+                            <div className="card-footer">
+                                <input type="text" placeholder="Username" value={this.state.username} onChange={e => this.setState({ username: e.target.value })} className="form-control" />
+                                <br />
+                                <input type="text" placeholder="Message" className="form-control" value={this.state.message} onChange={e => this.setState({ message: e.target.value })} />
+                                <br />
+                                <button onClick={this.sendMessage} className="btn" style={{backgroundColor: 'blueviolet'}} >Send</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-                <form>
-                    <input type='text' name='name' id='name' placeholder='Enter Name' onChange={this.handleChange} />
-                    <textarea type='text' name='message' id='message' placeholder='Type Message' onChange={this.handleChange} />
-                    <button type='submit'>Send</button> 
-                </form>
-            </div>
-        )
+        );
     }
 }
+
+ 
